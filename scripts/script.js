3aac182953ed6d0c853008eda3ac0e8d14f6aaee
salomenaomi <90864999+salomenaomi@users.noreply.github.com>
2021-11-02 13:23:48 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,26 @@
-const button = document.querySelector("button");
-const nav = document.querySelector("nav");
+const button = document.querySelector("#button");
+const nav = document.querySelector('nav');
+
+const menuButton = "images/hamburgermenu.png";
+const kruisje = "images/kruisje.png";
+const aantalKeerKlikken = 0;
 
 button.addEventListener("click", hamburgerMenu);
+button.addEventListener("click", hamburgerMenuVeranderen);
 
 function hamburgerMenu () {
-  nav.classList.add('active');
+  nav.classList.toggle('uitschuiven');
   console.log("Ik ben geklikt");
 }
 
-console.log("Hello World");
+// function hamburgerMenuVeranderen() {
+//   button.src = menuButton;
+//   aantalKeerKlikken++;
+//   if (aantalKeerKlikken % 2 != 0); {
+//     button.src = kruisje;
+//     console.log("Ik ben anders geklikt");
+//   } else (aantalKeerKlikken % 2 = 0); {
+//     button.src = menuButton;
+
+// }
+// }
